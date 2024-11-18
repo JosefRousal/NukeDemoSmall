f3fd5f2718880a7ae91edd76214ba2f846b0caf1
Josef Rousal-Crichton <josef.crichton@gmail.com>
2024-11-17 19:46:24 -0500

    add pack

--- build/Build.cs
+++ build/Build.cs
@@ -8,12 +8,13 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Octopus;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
-[TeamCity(Version = "2024.03", ManuallyTriggeredTargets = [nameof(Compile)], ImportSecrets = [nameof(EsriApiKey)])]
+[TeamCity(Version = "2024.03", ManuallyTriggeredTargets = [nameof(Compile), nameof(Pack)], ImportSecrets = [nameof(EsriApiKey)])]
 [TeamCityToken(nameof(EsriApiKey), "25f663b7-67d1-4103-80db-48b7f7ca69ff")]
 class Build : NukeBuild
 {
@@ -22,7 +23,7 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Pack);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -30,6 +31,9 @@ class Build : NukeBuild
     [Parameter, Secret] readonly string EsriApiKey;
 
     [Solution] readonly Solution Solution;
+
+    readonly AbsolutePath BuildOutputDirectory = RootDirectory / "build" / "output";
+    readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
 
     Project GetProject()
     {
@@ -56,6 +60,18 @@ class Build : NukeBuild
         .Executes(() =>
         {
             DotNetTasks.DotNetBuild(_ => _
-                .SetProjectFile(GetProject()));
+                .SetProjectFile(GetProject())
+                .SetOutputDirectory(BuildOutputDirectory));
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Produces(ArtifactsDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            OctopusTasks.OctopusPack(_ => _
+                .SetId("api")
+                .SetBasePath(BuildOutputDirectory)
+                .SetOutputFolder(ArtifactsDirectory));
         });
 }
